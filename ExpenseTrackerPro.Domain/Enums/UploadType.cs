cb1bb8ca84ed358ac507ca7f5211735ed2e970f4
NatePascual <nathan.pascual20@yahoo.com>
2023-12-25 03:36:23 +0800

    added groupings and added images

--- ExpenseTrackerPro.Domain/Enums/UploadType.cs
+++ ExpenseTrackerPro.Domain/Enums/UploadType.cs
@@ -22,3 +22,4 @@ public enum UploadType
     [Description(@"ImageTransactions\Income")]
     Income,
 }
+
---
+++ ExpenseTrackerPro.Shared/Enums/ImageLocation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ExpenseTrackerPro.Shared.Enums;
+
+public static class ImageLocation
+{
+    public static string AccountType { get; } = @"Images/AccountType/";
+    public static string Institution { get; } = @"Images/Institution/";
+    public static string Category { get; } = @"Images/Category/";
+    public static string IncomeCategory { get; } = @"Images/IncomeCategory/";
+    public static string Expense { get; } = @"ImageTransactions/Expense/";
+    public static string Income { get; } = @"ImageTransactions/Income/";
+}
