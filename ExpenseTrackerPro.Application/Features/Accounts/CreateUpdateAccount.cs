5cc4204e9668a02335ed0159dbecc5187fe6aea9
NatePascual <nathan.pascual20@yahoo.com>
2023-12-29 14:16:58 +0800

    added back the Update for Accounts except balance.

--- ExpenseTrackerPro.Application/Features/Accounts/CreateUpdateAccount.cs
+++ ExpenseTrackerPro.Application/Features/Accounts/CreateUpdateAccount.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations;
 using ExpenseTrackerPro.Application.Features.Transactions;
 using ExpenseTrackerPro.Shared.Enums;
 using ExpenseTrackerPro.Application.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTrackerPro.Application.Features.Accounts;
 
@@ -39,6 +40,7 @@ internal sealed class CreateUpdateAccountCommandHandler : IRequestHandler<Create
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private string _message;
     public CreateUpdateAccountCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
@@ -49,46 +51,76 @@ internal sealed class CreateUpdateAccountCommandHandler : IRequestHandler<Create
     {
         Result<int> result = null;
 
-        if(command.Id == 0)
+        var validate = await Validate(command, cancellationToken);
+
+        if (!validate)
+        {
+           return await Result<int>.FailAsync(_message);
+        }
+
+        if (command.Id == 0)
         {
             var entity = _mapper.Map<Account>(command);
             await _unitOfWork.Repository<Account>().AddAsync(entity);
             await _unitOfWork.Commit(cancellationToken);
 
             await ManageTransaction.AddAsync(_unitOfWork, entity.Id, TransactionType.StartingBalance.ToDescriptionString(),
-                                              entity.Created, entity.Balance, false, false,cancellationToken);
+                                                entity.Created, entity.Balance, false, false, cancellationToken);
 
-            result = await Result<int>.SuccessAsync(entity.Id,Messages.AccountSaved.ToDescriptionString());     
+            result = await Result<int>.SuccessAsync(entity.Id, Messages.AccountSaved.ToDescriptionString());
         }
         else
         {
-            var account = await _unitOfWork.Repository<Account>().GetByIdAsync(command.Id);
-
-            if(account != null)
-            {
-                account.AccountTypeId = (command.AccountTypeId == 0)  ? account.AccountTypeId : command.AccountTypeId;
-                account.InstitutionId = (command.InstitutionId == 0) ? account.InstitutionId : command.InstitutionId;
-                account.CurrencyId = (command.CurrencyId ==0) ? account.CurrencyId : command.CurrencyId;
-                account.Name = command.Name ?? account.Name;
-                account.AccountNumber = command.AccountNumber ?? account.AccountNumber;
-                account.Balance = (command.Balance == 0) ? account.Balance : command.Balance;  
-
-                var entity = _mapper.Map<Account>(account);
-                await _unitOfWork.Repository<Account>().UpdateAsync(entity);
-                await _unitOfWork.Commit(cancellationToken);
-               
-                result = await Result<int>.SuccessAsync(entity.Id, Messages.AccountUpdated.ToDescriptionString());
-            }
-            else
-            {
-                result = await Result<int>.FailAsync(Messages.RecordNotFound.ToDescriptionString());
-            }
+            result = await UpdateAccount(command, cancellationToken);
         }
-        
 
+            
         return result;
 
     }
 
+    private async Task<Result<int>> UpdateAccount(CreateUpdateAccountCommand command,CancellationToken cancellationToken)
+    {
+        Result<int> result = null;
 
+        var account = await _unitOfWork.Repository<Account>().GetByIdAsync(command.Id);
+
+        //Disabling balance for the meantime
+        if (account != null)
+        {
+            account.AccountTypeId = (command.AccountTypeId == 0) ? account.AccountTypeId : command.AccountTypeId;
+            account.InstitutionId = (command.InstitutionId == 0) ? account.InstitutionId : command.InstitutionId;
+            account.CurrencyId = (command.CurrencyId == 0) ? account.CurrencyId : command.CurrencyId;
+            account.Name = command.Name ?? account.Name;
+            account.AccountNumber = command.AccountNumber ?? account.AccountNumber;
+            //account.Balance = (command.Balance == 0) ? account.Balance : command.Balance;
+
+            var entity = _mapper.Map<Account>(account);
+            await _unitOfWork.Repository<Account>().UpdateAsync(entity);
+            await _unitOfWork.Commit(cancellationToken);
+
+            result = await Result<int>.SuccessAsync(entity.Id, Messages.AccountUpdated.ToDescriptionString());
+        }
+        else
+        {
+            result = await Result<int>.FailAsync(Messages.RecordNotFound.ToDescriptionString());
+        }
+
+        return result;
+    }
+
+    private async Task<bool> Validate(CreateUpdateAccountCommand command, CancellationToken cancellationToken)
+    {
+        var account =  _unitOfWork.Repository<Account>().Entities
+                       .Include(x=>x.Institution)
+                       .FirstOrDefault(x => x.AccountNumber == command.AccountNumber 
+                                         && x.InstitutionId == command.InstitutionId);
+
+        if (account != null)
+        {
+            _message = $"Account with Institution {account.Institution.Name} and Number {account.AccountNumber} already exists!";
+            return false;
+        }
+        return true;
+    }
 }
